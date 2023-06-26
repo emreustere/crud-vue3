4c20b21ef687d7b5356af7ce8270ecef2650e0ad
rippy56 <ustereemre@gmail.com>
2023-06-27 02:00:45 +0300

    Backend is completed.

--- server/db/posts.js
+++ server/db/posts.js
@@ -1,20 +1,20 @@
-import mongoose, { mongo } from "mongoose"
+import mongoose, { mongo } from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content:{
-        type: String,
-        required: true
-    },
-    creator:{
-        type:String,
-        required:true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+});
 
-const Model = mongoose.model('post', postSchema)
+const Post = mongoose.model("post", postSchema);
 
-export default Model
+export default Post;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-// import router from "./router/router.js"
-const app = express()
-app.use(cors())
-app.use(express.json())
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./router/router.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 
-// app.use('/posts',router)
+app.use("/posts", router);
 
 app.listen(process.env.PORT, () => {
-    mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser: true,
-             useUnifiedTopology: true
-    }).then(console.log("Connected to DB."))
-    .catch(err => console.log(err))
-})
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("Connected to DB."))
+    .catch((err) => console.log(err));
+});
